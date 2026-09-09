6bcfd788ad31b19ec519db5201ec93319fe06087
ChukanovaEkaterina <katerinaluzhnykh25@mail.ru>
2026-09-09 15:07:46 +0300

    add TestData and Attach

---
+++ src/test/java/testdata/TestData.java
@@ -0,0 +1,10 @@
+package testdata;
+
+import com.github.javafaker.Faker;
+
+public class TestData {
+
+    public  String login = "katerinaluzhnykh25@mail.ru";
+    public  String password = "katya1";
+    public  String textSuccessfulAuthorizationForm = "История заказов";
+}
