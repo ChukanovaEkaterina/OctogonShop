836a088db223cb9e1d6506a224b3554f9c360ad5
ChukanovaEkaterina <katerinaluzhnykh25@mail.ru>
2026-09-09 15:07:08 +0300

    add TestBase

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,62 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import pages.AuthorizationTestPage;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+
+    AuthorizationTestPage authorizationTestPage = new AuthorizationTestPage();
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @BeforeAll
+    static void setupEnvironment() {
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://octagon-shop.com");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserSize = System.getProperty( "browserSize", "1920x1080");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        String remoteUrl = System.getProperty("remoteUrl");
+        if (remoteUrl == null || remoteUrl.isEmpty() || "null".equals(remoteUrl)) {
+            remoteUrl = "https://user1:1234@selenoid.qa.guru/wd/hub";
+        }
+        Configuration.remote = remoteUrl;
+
+        System.out.println("baseUrl: " + Configuration.baseUrl);
+        System.out.println("browser: " + Configuration.browser);
+        System.out.println("browserSize: " + Configuration.browserSize);
+        System.out.println("browserVersion: " + Configuration.browserVersion);
+        System.out.println("headless: " + Configuration.headless);
+        System.out.println("remoteUrl: " + remoteUrl);
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
