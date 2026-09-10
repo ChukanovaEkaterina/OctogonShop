f07d728bdbc22647e4e0da74cbe0d000b1a080b0
ChukanovaEkaterina <katerinaluzhnykh25@mail.ru>
2026-09-10 15:56:35 +0300

    edit TestData

--- src/test/java/testdata/TestData.java
+++ src/test/java/testdata/TestData.java
@@ -1,10 +1,23 @@
 package testdata;
 
+import com.codeborne.selenide.SelenideElement;
 import com.github.javafaker.Faker;
+
+import static com.codeborne.selenide.Selenide.$;
 
 public class TestData {
 
-    public  String login = "katerinaluzhnykh25@mail.ru";
-    public  String password = "katya1";
+    Faker faker = new Faker();
+
+    public  String name = faker.name().fullName();
+    public  String email = faker.internet().emailAddress();
+    public  String phone = faker.phoneNumber().subscriberNumber(12);
+    public  String language = faker.options().option("English", "Русский(Russian)");
+    public  String password = faker.internet().password();
     public  String textSuccessfulAuthorizationForm = "История заказов";
+
+    private final SelenideElement passwordConfirmationInput = $("#client_password_confirmation");
+
+
+
 }
